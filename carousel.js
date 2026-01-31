9fe631b864f520b420aaf281657470eb3782a685
mklbasist <mukulch0001@gmail.com>
2026-01-31 18:55:05 +0530

    fix issue

--- carousel.js
+++
@@ -1,9 +0,0 @@
-const track = document.querySelector(".carousel-track");
-
-document.querySelector(".next").onclick = () => {
-  track.scrollBy({ left: 300, behavior: "smooth" });
-};
-
-document.querySelector(".prev").onclick = () => {
-  track.scrollBy({ left: -300, behavior: "smooth" });
-};
