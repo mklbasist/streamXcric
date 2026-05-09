eb6cf51cbabe55f7037bf2fd074f5891d21a84c5
mklbasist <145832525+mklbasist@users.noreply.github.com>
2026-05-09 10:35:32 +0530

    Change back button redirect to main section

--- series.js
+++ series.js
@@ -84,6 +84,6 @@ document.addEventListener("DOMContentLoaded", async () => {
 const backBtn = document.getElementById("seriesBackBtn");
 if (backBtn) {
   backBtn.addEventListener("click", () => {
-    window.location.href = "index.html#matches";
+    window.location.href = "index.html#main";
   });
 }
